7250f1b12c6c9a993802f0cc54d3ef3e5e8fdf46
Dennis Burger <dennis@dutchwebworks.nl>
2022-12-25 16:28:38 +0100

    Updated config of NetlifyCMS scripts

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  trailingSlash: 'never',
   site: "https://astro-learning.dutchwebworks.nl",
   integrations: [tailwind(), sitemap()],
 });
