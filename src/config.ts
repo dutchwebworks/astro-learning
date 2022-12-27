9b3514b6f7d39fee780c91b5b3ec4b99e37a53e6
Dennis Burger <dennis@dutchwebworks.nl>
2022-12-27 19:22:22 +0100

    Fixed typo

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Dutchwebworks Atro Learning";
+export const SITE_TITLE = "Dutchwebworks Astro Learning";
 export const SITE_DESCRIPTION = "Welcome to my website!";
 export const BLOG_PATH = "blog";
